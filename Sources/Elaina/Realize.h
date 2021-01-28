fd5cab7e3e4229ed7335650437f4b2cc52ba6f98
SeolYang <kyowonhorang@gmail.com>
2021-01-29 07:11:10 +0900

    Realize 구현을 위한 템플릿 메서드를 추가하였습니다

---
+++ Sources/Elaina/Realize.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <Elaina/Elaina.config.h>
+
+namespace Elaina
+{
+   template <typename DescriptorType, typename ActualType>
+   struct MissingRealizeImplementation : std::false_type
+   {
+      /** static assert를 이용한 더욱 정확한 compile time error */
+   };
+
+   template <typename DescriptorType, typename ActualType>
+   ActualType* Realize(const DescriptorType& descriptor)
+   {
+      static_cast(MissingRealizeImplementation<DescriptorType, ActualTyle>::value, "주어진 타입에 대한 Realize의 구현을 찾을 수 없습니다.");
+      return nullptr;
+   }
+}
