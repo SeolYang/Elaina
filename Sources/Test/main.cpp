ea7bf1ad470d607c06bac651b14a08970cd24032
SeolYang <kyowonhorang@gmail.com>
2021-01-29 07:21:31 +0900

    Realize method test code

--- Sources/Test/main.cpp
+++ Sources/Test/main.cpp
@@ -2,7 +2,33 @@
 #include <Elaina/FrameGraphBuilder.h>
 #include <Elaina/FrameResource.h>
 
+struct IntDescriptor
+{
+   int Value = 0;
+};
+
+using IntFrameResource = Elaina::FrameResource<IntDescriptor, int>;
+
+struct FloatDescriptor
+{
+   float Value = 0.0f;
+};
+
+using FloatFrameResource = Elaina::FrameResource<FloatDescriptor, float>;
+
+template<>
+int* Elaina::Realize(const IntDescriptor& descriptor)
+{
+   return new int(descriptor.Value);
+}
+
 int main()
 {
+   auto* realized = Elaina::Realize<IntDescriptor, int>({ 5 });
+   Elaina::SafeDelete(realized);
+
+   // auto* missingRealized = Elaina::Realize<FloatDescriptor, float>({ 3.0f }); /** Missing impelemntation example */
+   // Elaina::SafeDelete(missingRealized);
+
    return 0;
 }
