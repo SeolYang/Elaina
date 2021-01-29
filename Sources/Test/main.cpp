59f639a19f17ef800766b76de5457ab791834004
SeolYang <kyowonhorang@gmail.com>
2021-01-30 03:12:10 +0900

    FrameGraph를 이용한 간단한 예시를 구현하였습니다

--- Sources/Test/main.cpp
+++ Sources/Test/main.cpp
@@ -1,5 +1,10 @@
+#include <iostream>
 #include <Elaina/FrameGraph.h>
 #include <Elaina/FrameResource.h>
+
+#ifdef _DEBUG
+#include <crtdbg.h>
+#endif
 
 struct IntDescriptor
 {
@@ -21,13 +26,57 @@ int* Elaina::Realize(const IntDescriptor& descriptor)
    return new int(descriptor.Value);
 }
 
+template<>
+float* Elaina::Realize(const FloatDescriptor& descriptor)
+{
+   return new float(descriptor.Value);
+}
+
+struct FloatIntAddPassData
+{
+   IntFrameResource* IntegerInput;
+   FloatFrameResource* FloatInput;
+   FloatFrameResource* Output;
+};
+
 int main()
 {
-   auto* realized = Elaina::Realize<IntDescriptor, int>({ 5 });
-   Elaina::SafeDelete(realized);
+#ifdef _DEBUG
+   _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
 
-   // auto* missingRealized = Elaina::Realize<FloatDescriptor, float>({ 3.0f }); /** Missing impelemntation example */
-   // Elaina::SafeDelete(missingRealized);
+   Elaina::FrameGraph frameGraph;
 
+   float* output = new float();
+   auto externalPermanentResource =
+      frameGraph.AddExternalPermanentResource("Output", FloatDescriptor(), output);
+
+   auto renderPass = frameGraph.AddCallbackPass<FloatIntAddPassData>(
+      /** Render Pass Name */
+      "AddPass",
+      /** Setup callback */
+      [&](Elaina::RenderPassBuilder& builder, FloatIntAddPassData& data)
+      {
+         data.IntegerInput = builder.Create<IntFrameResource>("Integer0", IntDescriptor{ 3 });
+         data.FloatInput = builder.Create<FloatFrameResource>("Float0", FloatDescriptor{ 2.0f });
+         data.Output = builder.Write(externalPermanentResource);
+      },
+      /** Execute callback  */
+      [](const FloatIntAddPassData& data)
+      {
+         auto IntegerInputActual = data.IntegerInput->GetActual();
+         auto FloatInputActual = data.FloatInput->GetActual();
+         auto OutputActual = data.Output->GetActual();
+         (*OutputActual) = static_cast<float>(*IntegerInputActual) + (*FloatInputActual);
+      });
+
+   auto data = renderPass->GetData();
+
+   frameGraph.Compile();
+   frameGraph.Execute();
+
+   std::cout << (*data.Output->GetActual()) << std::endl;
+
+   Elaina::SafeDelete(output);
    return 0;
 }
